3b5f3fc6aead1e94ca20121238c8327143225187
rafaelitajahy <rafaelitajahy@gmail.com>
2019-09-30 21:59:48 -0300

    finished project

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     'react',
     'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -61,6 +62,9 @@ module.exports = {
       ],
       "depth": 3,
     }],
-    "no-console": ["error", {allow: ["tron"]}]
+    "no-console": ["error", {allow: ["tron"]}],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
